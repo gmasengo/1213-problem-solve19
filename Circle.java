1e0dd078904978c525218ece4094e133e56dba58
gmasengo <guylain2010@gmail.com>
2025-07-22 16:20:32 -0400

    update everythinh

--- Circle.java
+++ Circle.java
@@ -2,46 +2,87 @@ import java.awt.Color;
 
 /**
  * This class describes a circle with a given radius and color.
- *
- * @author nanajjar
- * @version Spring 25
- */
+ *This is my private solutionto problem Solve 19
+ * @author Guylain Masengo
+ * 
+ * @version 2025 July 15
+  */
 
-public class Circle {  
-    // private instance variable, not accessible from outside this class
+public class Circle { 
+    
+    /**
+     * // private instance variable, not accessible from outside this class
+     */
+    
     private double radius;
     private Color color;
     private double area;
 
-    // The default constructor with no argument.
-    // It sets the radius and color to their default value.
+
+
+    /**
+     * 
+    /  The default constructor with no argument.
+    / It sets the radius to 1 and color to red
+     */
+
+    
+
     public Circle() {
         radius = 1.0;
         this.color = new Color(255,119,65);
     }
+    
 
-    // 2nd constructor with given radius and color
+
+
+
+    /**
+     *  2nd constructor with given radius and color
+     * @param the r of the circle;
+     * @param the clr of the circle 
+     */
+    
     public Circle(double r, Color clr) {
         radius = r;
         color = clr;
     }
 
-    // A public method for retrieving the radius
+    /**
+     *  A public method for retrieving the radius
+     * @return radius;
+     */
+    
     public double getRadius() {
         return radius;
     }
 
-    // A public method for retrieving the color of circle
+
+
+    /**
+     * A public method for retrieving the color of circle
+     * @return  color
+     */
+    
     public Color getColor() {
         return color;
     }
 
-    // A public method for retrieving the area of circle
+
+    /**
+     * A public method for retrieving the area of circle
+     * @return area
+     */
+    
     public double getArea() {
         calculateArea();
         return area;
     }
-    // A private method for computing the area of circle
+
+    /**
+     * A private method for computing the area of circle
+     */
+     
     private void calculateArea() {
         area = radius * radius * Math.PI;
     }
--- RectangleTest.java
+++ RectangleTest.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 import java.awt.Rectangle;
+
 
 public class RectangleTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(10, 10, 40, 30);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter height: ");
+        int width = input.nextInt();
+        System.out.print("Enter width: ");
+        int height = input.nextInt();
+        Rectangle box1 = new Rectangle(10, 10, 30, 40);
+        Rectangle box2 = new Rectangle(10,10, width , height);
         System.out.println("box1: " + box1);
+
+         int expectedArea = width * height;
+         int area = width * height;
+         if (area == expectedArea) {
+     System.out.println("++++ Test passed: Area is correct.");
+ } else {
+     System.out.println("---- Test failed: Expected " + expectedArea + " but got " + area);
+ }
     }
 
 }
